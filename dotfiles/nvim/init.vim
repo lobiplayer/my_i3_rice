f9a73f4fce9e9b0bc223a0d585e579b7f1eef8f9
Robin Mohan <jrobinmohan@gmail.com>
2022-12-09 00:07:31 +0100

    nvim plugin add

---
+++ dotfiles/nvim/init.vim
@@ -0,0 +1,10 @@
+:set number
+:set autoindent
+:set smarttab
+:set mouse=a
+
+call plug#begin()
+
+Plug 'https://github.com/ap/vim-css-color' " CSS Color Preview
+
+call plug#end()
--- install_my_rice.sh
+++ install_my_rice.sh
@@ -3,67 +3,71 @@
 ####################### INSTALLATIONS ###################################
 
 # dnf update
-dnf update -y
+sudo dnf update -y
 
 # Install Git
-dnf install git -y
+sudo dnf install git -y
 
 # Install pip
-dnf install pip -y
+sudo dnf install pip -y
 
 # Install Polybar
-dnf install polybar -y
+sudo dnf install polybar -y
 
 #Install needed fonts for Polybar
-dnf install jetbrains-mono-nl-fonts -y
+sudo dnf install jetbrains-mono-nl-fonts -y
 #dnf copr enable peterwu/iosevka -y
 #dnf install iosevka-term-fonts.noarchi -y
-mkdir -p /home/rmohan/.local/share/fonts/
+mkdir -p ~/.local/share/fonts/
 wget -O https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Iosevka/Regular/complete/Iosevka%20Nerd%20Font%20Complete.ttf -P /home/rmohan/.local/share/fonts/
 fc-cache -f -v
 
 # Install i3-gaps
-dnf install i3-gaps -y --allowerasing
+sudo dnf install i3-gaps -y --allowerasing
 
 # Install neovim
-dnf install neovim python3-neovim -y
+sudo dnf install neovim python3-neovim -y
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 
 # Set neovim als default editor for user
 if grep -q "export EDITOR=nvim" "/home/rmohan/.bash_profile"; then 
     echo "Exists" 
 else 
     echo "Does not exist"
-    echo export EDITOR="nvim" >>  /home/rmohan/.bash_profile
+    echo export EDITOR="nvim" >>  ~/.bash_profile
 fi
 
 # Install Picom
-dnf install picom -y
+sudo dnf install picom -y
 
 # Install Fish
-dnf install fish -y
-dnf install util-linux-user -y
-chsh -s /usr/bin/fish
-chsh -s /usr/bin/fish rmohan
+sudo dnf install fish -y
+sudo dnf install util-linux-user -y
+sudo chsh -s /usr/bin/fish
+sudo chsh -s /usr/bin/fish rmohan
 
 
 # Install Alacritty
-dnf install alacritty -y
+sudo dnf install alacritty -y
 # Install completions for fish
 #mkdir -p $fish_complete_path[1]
 #cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
 
 # Install PyWal
-pip3 install pywal
+sudo pip3 install pywal
 
 # Install feh (for wallpapers)
-dnf install feh -y
+sudo dnf install feh -y
 
 # Install Qutebrowser
 
-dnf install qutebrowser -y
+sudo dnf install qutebrowser -y
 
 # Install ranger
-dnf install ranger -y
+sudo dnf install ranger -y
 
 # Install and run OhMyFish
 
